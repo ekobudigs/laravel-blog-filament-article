df14e1b2aa52ca9966a4348a1b126804959547c3
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-05-30 18:03:55 +0700

    update post controller and styleng posting

---
+++ resources/views/components/post-item.blade.php
@@ -0,0 +1,10 @@
+<article class="flex flex-col shadow my-4">
+    <!-- Article Image -->
+    <a href="#" class="hover:opacity-75">
+        <img src="{{ $post->thumnail }}">
+    </a>
+    <div class="bg-white flex flex-col justify-start p-6">
+        <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">Technology</a>
+        
+    </div>
+</article>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,53 +1,12 @@
-<!-- Posts Section -->
+<x-app-layout>
+    <!-- Posts Section -->
 <section class="w-full md:w-2/3 flex flex-col items-center px-3">
 
-    <article class="flex flex-col shadow my-4">
-        <!-- Article Image -->
-        <a href="#" class="hover:opacity-75">
-            <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=1">
-        </a>
-        <div class="bg-white flex flex-col justify-start p-6">
-            <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">Technology</a>
-            <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</a>
-            <p href="#" class="text-sm pb-3">
-                By <a href="#" class="font-semibold hover:text-gray-800">David Grzyb</a>, Published on April 25th, 2020
-            </p>
-            <a href="#" class="pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet posuere magna..</a>
-            <a href="#" class="uppercase text-gray-800 hover:text-black">Continue Reading <i class="fas fa-arrow-right"></i></a>
-        </div>
-    </article>
+    @foreach ($posts as $post )
+        <x-post-item :post="$post"> </x-post-item>
+    @endforeach
 
-    <article class="flex flex-col shadow my-4">
-        <!-- Article Image -->
-        <a href="#" class="hover:opacity-75">
-            <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=2">
-        </a>
-        <div class="bg-white flex flex-col justify-start p-6">
-            <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">Automotive, Finance</a>
-            <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</a>
-            <p href="#" class="text-sm pb-3">
-                By <a href="#" class="font-semibold hover:text-gray-800">David Grzyb</a>, Published on January 12th, 2020
-            </p>
-            <a href="#" class="pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet posuere magna..</a>
-            <a href="#" class="uppercase text-gray-800 hover:text-black">Continue Reading <i class="fas fa-arrow-right"></i></a>
-        </div>
-    </article>
-
-    <article class="flex flex-col shadow my-4">
-        <!-- Article Image -->
-        <a href="#" class="hover:opacity-75">
-            <img src="https://source.unsplash.com/collection/1346951/1000x500?sig=3">
-        </a>
-        <div class="bg-white flex flex-col justify-start p-6">
-            <a href="#" class="text-blue-700 text-sm font-bold uppercase pb-4">Sports</a>
-            <a href="#" class="text-3xl font-bold hover:text-gray-700 pb-4">Lorem Ipsum Dolor Sit Amet Dolor Sit Amet</a>
-            <p href="#" class="text-sm pb-3">
-                By <a href="#" class="font-semibold hover:text-gray-800">David Grzyb</a>, Published on October 22nd, 2019
-            </p>
-            <a href="#" class="pb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus quis porta dui. Ut eu iaculis massa. Sed ornare ligula lacus, quis iaculis dui porta volutpat. In sit amet posuere magna..</a>
-            <a href="#" class="uppercase text-gray-800 hover:text-black">Continue Reading <i class="fas fa-arrow-right"></i></a>
-        </div>
-    </article>
+  
 
     <!-- Pagination -->
     <div class="flex items-center py-8">
@@ -57,4 +16,5 @@
     </div>
 
 </section>
+</x-app-layout>
  
