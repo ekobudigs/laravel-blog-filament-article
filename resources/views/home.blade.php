1e624ab877c416e88c540da70c46ebf6b2d516ae
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-06-05 17:56:09 +0700

    add pagination links and function

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,3 +1,7 @@
+<?php 
+  /**  @var $posts \Illuminate\Pagination\LengthAwarePaginator */
+?>
+
 <x-app-layout>
     <!-- Posts Section -->
 <section class="w-full md:w-2/3 flex flex-col items-center px-3">
@@ -6,14 +10,9 @@
         <x-post-item :post="$post"> </x-post-item>
     @endforeach
 
-  
+    {{ $posts->onEachSide(1)->links() }}
 
-    <!-- Pagination -->
-    <div class="flex items-center py-8">
-        <a href="#" class="h-10 w-10 bg-blue-800 hover:bg-blue-600 font-semibold text-white text-sm flex items-center justify-center">1</a>
-        <a href="#" class="h-10 w-10 font-semibold text-gray-800 hover:bg-blue-600 hover:text-white text-sm flex items-center justify-center">2</a>
-        <a href="#" class="h-10 w-10 font-semibold text-gray-800 hover:text-gray-900 text-sm flex items-center justify-center ml-3">Next <i class="fas fa-arrow-right ml-2"></i></a>
-    </div>
+
 
 </section>
 </x-app-layout>
