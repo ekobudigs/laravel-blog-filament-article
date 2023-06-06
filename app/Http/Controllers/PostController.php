cc512919ea708bf8326d47b729af33b89f046741
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2023-06-06 17:56:58 +0700

    view post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class PostController extends Controller
 {
@@ -41,9 +42,13 @@ class PostController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Post $post)
     {
-        //
+        if(!$post->active || $post->published_at> Carbon::now()){
+            throw new NotFoundHttpException();
+        }
+
+        return view('post.view', compact('post'));
     }
 
     /**
