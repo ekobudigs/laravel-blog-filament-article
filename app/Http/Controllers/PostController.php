ff2d64af4dacd9a73b26d3f94639c5dc88651aef
MOCH EKO BUDI SETIAWAN <86825878+ekobudigs@users.noreply.github.com>
2024-06-05 11:44:03 +0700

    update

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -40,7 +40,7 @@ class PostController extends Controller
             ->where('active', '=', 1)
             ->whereDate('published_at', '<', Carbon::now())
             ->orderByDesc('upvote_count')
-            ->groupBy('posts.id', 'posts.title', 'posts.slug')
+            ->groupBy('posts.id', 'posts.title', 'posts.slug', 'posts.thumnail')
             ->limit(3)
             ->get();
 
